d9eb6eb65058b85851776b75180e7b15aac0c7c9
indraargamuria <indraargaaa@gmail.com>
2023-03-01 17:54:32 +0700

    coba menambahkan test page pada branch berbeda

---
+++ src/userpages/TestPage.jsx
@@ -0,0 +1,11 @@
+import React, { Component, Fragment } from 'react';
+// import './TryOut.css';
+
+class TestPage extends Component {
+    render(){
+        return <Fragment>
+        </Fragment>
+    }
+}
+
+export default TestPage;
